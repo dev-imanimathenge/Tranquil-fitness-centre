a255c0268cd58c80e691fc9b30de4a504436dfae
dev-imanimathenge <imani.gachuiri@strathmore.edu>
2026-06-28 19:28:13 +0300

    Implement dark mode toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,3 +106,22 @@ if (filterButtons.length > 0) {
         });
     });
 }
+
+const toggleBtn = document.getElementById('darkModeToggle');
+
+if (toggleBtn) {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+        toggleBtn.textContent = '☀️';
+    }
+    toggleBtn.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+            toggleBtn.textContent = '☀️';
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+            toggleBtn.textContent = '🌙';
+        }
+    });
+}
